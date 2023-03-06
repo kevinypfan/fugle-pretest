891d2cc2914dbe0422166b0977d6cfc35d78056a
KevinFan <kevinypfan@gmail.com>
2023-03-07 02:19:39 +0800

    move validate user ID to guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,8 @@
-import {
-  BadRequestException,
-  Controller,
-  Get,
-  HttpStatus,
-  Query,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { RateLimitGuard } from './rate-limit.guard';
 import { InjectRedis, Redis } from '@nestjs-modules/ioredis';
 import { TradeService } from './stream/trade.service';
-import { isNumeric } from './utils';
 
 @Controller()
 export class AppController {
@@ -28,20 +20,6 @@ export class AppController {
   @Get('/data')
   @UseGuards(RateLimitGuard)
   async getData(@Query('user') userId): Promise<object> {
-    console.log(userId);
-    if (!isNumeric(userId))
-      throw new BadRequestException({
-        statusCode: HttpStatus.BAD_REQUEST,
-        error: 'Bad Request',
-        message: 'User ID invalid',
-      });
-
-    if (+userId < 1 || +userId > 1000)
-      throw new BadRequestException({
-        statusCode: HttpStatus.BAD_REQUEST,
-        error: 'Bad Request',
-        message: 'User ID must be 1 ~ 1000',
-      });
     const result = await this.appService.getData();
     return { result };
   }
--- src/rate-limit.guard.ts
+++ src/rate-limit.guard.ts
@@ -4,9 +4,11 @@ import {
   ExecutionContext,
   HttpStatus,
   HttpException,
+  BadRequestException,
 } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import { InjectRedis, Redis } from '@nestjs-modules/ioredis';
+import { isNumeric } from './utils';
 // import { Observable } from 'rxjs';
 
 @Injectable()
@@ -15,14 +17,18 @@ export class RateLimitGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const { RL_TTL, RL_LIMIT_ID, RL_LIMIT_IP } = process.env;
-    console.log({ RL_TTL, RL_LIMIT_ID, RL_LIMIT_IP });
 
     const req = context.switchToHttp().getRequest();
 
     const userId = req.query.user;
     const ip = req.ip;
-    const userIdCount = await this.rlCheck({ key: userId, ttl: +RL_TTL });
+
     const ipCount = await this.rlCheck({ key: ip, ttl: +RL_TTL });
+
+    this.validateUserId(userId);
+
+    const userIdCount = await this.rlCheck({ key: userId, ttl: +RL_TTL });
+
     if (ipCount > +RL_LIMIT_IP || userIdCount > +RL_LIMIT_ID)
       throw new HttpException(
         {
@@ -49,5 +55,21 @@ export class RateLimitGuard implements CanActivate {
     const count = arr[1].length / 2;
 
     return count;
+  }
+
+  validateUserId(userId) {
+    if (!isNumeric(userId))
+      throw new BadRequestException({
+        statusCode: HttpStatus.BAD_REQUEST,
+        error: 'Bad Request',
+        message: 'User ID invalid',
+      });
+
+    if (+userId < 1 || +userId > 1000)
+      throw new BadRequestException({
+        statusCode: HttpStatus.BAD_REQUEST,
+        error: 'Bad Request',
+        message: 'User ID must be 1 ~ 1000',
+      });
   }
 }
